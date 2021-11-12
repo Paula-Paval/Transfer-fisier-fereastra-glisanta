d5863a15b5684a3a45a24dfdae6902a1ae42713a
Georgiana Chihaia <georgiana.chihaia@student.tuiasi.ro>
2021-11-12 12:32:14 +0200

    Interfata_grafica_Receiver - ConnectionToSender

---
+++ ConnectionToSender.py
@@ -0,0 +1,48 @@
+import socket
+import sys
+import select
+import threading
+
+from Interfata_grafica_Receiver import Receiver_ui
+
+
+def receive_fct():
+    global running
+    contor = 0
+    while running:
+        # Apelam la functia sistem IO -select- pentru a verifca daca socket-ul are date in bufferul de receptie
+        # Stabilim un timeout de 1 secunda
+        r, _, _ = select.select([s], [], [], 1)
+        if not r:
+        	contor = contor + 1
+        else:
+            data, address = s.recvfrom(1024)
+            print("S-a receptionat ", str(data), " de la ", address)
+            print("Contor= ", contor)
+
+#citire valori pentru conectare
+r=Receiver_ui("Receiver")
+r.afisare_IGR()
+sport=r.sport()
+dport=r.dport()
+dip=r.dip()
+print(sport)
+print(dport)
+print(dip)
+
+
+
+# Creare socket UDP
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+
+s.bind(('0.0.0.0', int(sport)))
+
+running = True
+
+try:
+    receive_thread = threading.Thread(target=receive_fct)
+    receive_thread.start()
+except:
+    print("Eroare la pornirea thread‐ului")
+    sys.exit()
+
