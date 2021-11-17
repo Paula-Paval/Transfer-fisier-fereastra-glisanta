627585f91b7a62c37581b208740224127d59b704
Paula Paval <paula.paval2000@gmail.com>
2021-11-17 21:54:17 +0200

    socket

---
+++ Socket.py
@@ -0,0 +1,48 @@
+import select
+import socket
+import sys
+import threading
+import sender_ui
+
+class Socket:
+    running=False
+
+    def creare(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        self.s.bind(('0.0.0.0', int(sender_ui.Sender_ui.porturi[0])))
+        print(sender_ui.Sender_ui.porturi[0])
+        self.running=True
+        try:
+            self.receive_thread = threading.Thread(target=self.receive_fct)
+            self.receive_thread.start()
+        except:
+            print("Eroare la pornirea thread‐ului")
+            sys.exit()
+
+    def receive_fct(self):
+        contor = 0
+        while self.running:
+            # Apelam la functia sistem IO -select- pentru a verifca daca socket-ul are date in bufferul de receptie
+            # Stabilim un timeout de 1 secunda
+            r, _, _ = select.select([self.s], [], [], 1)
+            if not r:
+                contor = contor + 1
+            else:
+                data, address = self.s.recvfrom(1024)
+                print("S-a receptionat ", str(data), " de la ", address)
+                print("Contor= ", contor)
+
+    def send_fct(self):
+        ##exceptie????
+        while True:
+            if(sender_ui.Sender_ui.text!="stop"):
+                data=sender_ui.Sender_ui.text
+                self.s.sendto(bytes(data, encoding="ascii"), (sender_ui.Sender_ui.dip, sender_ui.Sender_ui.porturi[1]))
+            else:
+                running = False
+                self.receive_thread.join()
+                break
+
+
+
+
--- sender_ui.py
+++ sender_ui.py
@@ -1,23 +1,32 @@
 import tkinter
+import time
+from threading import Thread
 from tkinter import *
 class Sender_ui:
+    porturi=[0, 0]
+    dip=""
+    text=""
     def __init__(self, nume):
         self.fereastra=Tk(className=nume)
 
-    def getConnectionInfo (self):
-        self.sport = int(self.entry1.get())
-        self.dport =int(self.entry2.get())
-        self.dip = self.entry3.get()
-        var = IntVar()
-        self.fereastra.after(3000, var.set, 1)
-        print("waiting...")
-        self.fereastra.wait_variable(var)
-        print("done")
+    def setConnectionInfo (self):
+        Sender_ui.porturi[0] = int(self.entry1.get())
+        Sender_ui.porturi[1] =int(self.entry2.get())
+        Sender_ui.dip = self.entry3.get()
+        print("sleep time start")
+
+        for i in range(10):
+            print(i)
+            time.sleep(1)
+
+        print("sleep time stop")
         return
+
     def getText(self):
-        self.text=self.entry4.get()
-        print(self.text)
+        Sender_ui.text=self.entry4.get()
+        print(Sender_ui.text)
         return
+
     def afisare(self):
         self.fereastra.geometry("500x500")
         self.l1 = Label(self.fereastra, text="Port sursa")
@@ -32,10 +41,16 @@ class Sender_ui:
         self.l3.place(x=80, y=80)
         self.entry3 = tkinter.Entry(self.fereastra, width="30")
         self.entry3.place(x=150, y=80)
-        bunttonSubmit=tkinter.Button(self.fereastra,text="Submit", command=self.getConnectionInfo ).place(x=300, y=110)
+        bunttonSubmit=tkinter.Button(self.fereastra,text="Submit", command=self.setConnectionInfo ).place(x=300, y=110)
         self.l4=Label(self.fereastra, text="Mesaj de trimis")
         self.l4.place(x=60, y=160)
         self.entry4=tkinter.Entry(self.fereastra,width="40")
         self.entry4.place(x=160, y=160)
         bunttonSend =tkinter.Button(self.fereastra,text="Send", command=self.getText).place(x=350, y=190)
         self.fereastra.mainloop()
+
+    def threading(self):
+        t1 = Thread(target=self.setConnectionInfo)
+        t1.start()
+
+
